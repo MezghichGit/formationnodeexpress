a8d9da2b37a4b887c5636fce7519777f1a5e5935
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-10-29 16:43:21 +0100

    version finale crud

--- index.js
+++ index.js
@@ -1,9 +1,73 @@
 const express = require('express');
-const equipesData = require('./equipes.json');
+//const equipesData = require('./equipes.json');
 const app = express();
 app.use(express.json());
+/// Connexion à la base de données
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const dbName = 'bdmonapi';
+let db 
+MongoClient.connect(url, function(err, client) {
+ db = client.db(dbName);
+console.log("Connexion réussi avec Mongo");
+});
+
+/// fin connexion
 app.listen(82,()=>console.log("Hello ExpressJS"));
 
+app.get('/equipes', (req,res) => {
+      db.collection('equipe').find({}).toArray(function(err, docs) {
+          if (err) {
+              console.log(err)
+              throw err
+          }
+          res.status(200).json(docs)
+        }) 
+    })
+    
+    app.get('/equipes/:id', async (req,res) => {
+          const id = parseInt(req.params.id)
+          try {
+              const docs = await db.collection('equipe').find({id}).toArray()
+              res.status(200).json(docs)
+          } catch (err) {
+              console.log(err)
+              throw err
+          }
+        })
+        app.post('/equipes', async (req,res) => {
+              try {
+                  const equipeData = req.body
+                  const equipe = await db.collection('equipe').insertOne(equipeData)
+                  res.status(200).json(equipe)
+              } catch (err) {
+                  console.log(err)
+                  throw err
+              }
+            })
+            app.put('/equipes/:id', async (req,res) => {
+                  try {
+                      const id = parseInt(req.params.id)
+                      const replacementEquipe = req.body
+                      const equipe = await db.collection('equipe').replaceOne({id},replacementEquipe)
+                      res.status(200).json(equipe)
+                  } catch (err) {
+                      console.log(err)
+                      throw err
+                  }
+                })
+                app.delete('/equipes/:id', async (req,res) => {
+                      try {
+                          const id = parseInt(req.params.id)
+                          const equipe = await db.collection('equipe').deleteOne({id})
+                          res.status(200).json(equipe)
+                      } catch (err) {
+                          console.log(err)
+                          throw err
+                      } 
+                    })
+                    
+/*
 app.get('/equipes',(req,res)=>{
     //res.send("<h1 align=center>Liste des équipes!<h1>");
     res.status(200).json(equipesData);
@@ -19,3 +83,18 @@ app.post('/equipes',(req,res)=>{
     equipesData.push(req.body);
     res.status(200).json(equipesData);
 });
+
+app.put('/equipes/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    const equipe = equipesData.find(equipe =>equipe.id===id);
+    equipe.name =req.body.name;
+    equipe.country =req.body.country;
+    res.status(200).json(equipe);
+});
+
+app.delete('/equipes/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    const equipe = equipesData.find(equipe =>equipe.id===id);
+    equipesData.splice(equipesData.indexOf(equipe),1);
+    res.status(200).json(equipesData);
+});*/
