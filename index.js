2621e76df0834a5fee77b57516faf2d73a567b6b
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-10-29 15:54:50 +0100

    request get id

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ app.get('/equipes',(req,res)=>{
     //res.send("<h1 align=center>Liste des équipes!<h1>");
     res.status(200).json(equipesData);
 });
+
+app.get('/equipes/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    const equipe = equipesData.find(equipe =>equipe.id===id);
+    res.status(200).json(equipe);
+});
