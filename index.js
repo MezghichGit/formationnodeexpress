b92b8445cc5ae2ad1455c0d95f1735bc843dd721
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-10-29 15:36:03 +0100

    request get equipes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-Hello Node Express
+const express = require('express');
+const equipes = require('./equipes.json');
+const app = express();
+app.listen(82,()=>console.log("Hello ExpressJS"));
+
+app.get('/equipes',(req,res)=>{
+    res.send("Liste des équipes!");
+});
